bcad1b3be4729a9060c611c109e458b88eac0dd4
VanoTole <toledano.ivan16@gmail.com>
2025-01-02 23:45:45 -0600

    Se añaden cambios de T02

--- oop-essentials/T02-load-data/run_regressor_evaluation.py
+++ oop-essentials/T02-load-data/run_regressor_evaluation.py
@@ -1,6 +1,5 @@
-from sklearn import metrics
+from sklearn import linear_model, metrics
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
@@ -8,25 +7,35 @@ from sklearn.tree import DecisionTreeRegressor
 from songpop.data import *
 
 
-def evaluate_model(model, X_train, y_train, X_test, y_test):
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    mae = metrics.mean_absolute_error(y_test, y_pred)
-    print(f"{model}: MAE={mae:.1f}")
-
-
 def main():
     dataset = Dataset(10000)
     X, y = dataset.load_xy_projected_scaled()
 
-    # split data
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.3, shuffle=True)
 
-    # evaluate models
-    evaluate_model(LogisticRegression(solver='lbfgs', max_iter=1000), X_train, y_train, X_test, y_test)
-    evaluate_model(KNeighborsRegressor(n_neighbors=1), X_train, y_train, X_test, y_test)
-    evaluate_model(RandomForestRegressor(n_estimators=100), X_train, y_train, X_test, y_test)
-    evaluate_model(DecisionTreeRegressor(random_state=42, max_depth=2), X_train, y_train, X_test, y_test)
+    log_reg = linear_model.LogisticRegression(solver='lbfgs', max_iter=1000)
+    log_reg.fit(X_train, y_train)
+    y_pred = log_reg.predict(X_test)
+    mae = metrics.mean_absolute_error(y_test, y_pred)
+    print(f"LogisticRegression: MAE={mae:.1f}")
+
+    knn = KNeighborsRegressor(n_neighbors=1)
+    knn.fit(X_train, y_train)
+    y_pred = knn.predict(X_test)
+    mae = metrics.mean_absolute_error(y_test, y_pred)
+    print(f"KNeighborsRegressor: MAE={mae:.1f}")
+
+    rforest = RandomForestRegressor(n_estimators=100)
+    rforest.fit(X_train, y_train)
+    y_pred = rforest.predict(X_test)
+    mae = metrics.mean_absolute_error(y_test, y_pred)
+    print(f"RandomForestRegressor: MAE={mae:.1f}")
+
+    d_tree = DecisionTreeRegressor(random_state=42, max_depth=2)
+    d_tree.fit(X_train, y_train)
+    y_pred = d_tree.predict(X_test)
+    mae = metrics.mean_absolute_error(y_test, y_pred)
+    print(f"DecisionTreeRegressor: MAE={mae:.1f}")
 
 
 if __name__ == '__main__':
