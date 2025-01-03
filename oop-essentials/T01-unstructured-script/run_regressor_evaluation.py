2e81b022951de1841def7e80f5a4345c0e15acf6
VanoTole <toledano.ivan16@gmail.com>
2025-01-02 18:35:33 -0600

    se agrega gitignore, nuevo script T01, los datos sampleados, y ambiente de trabajo

--- oop-essentials/T01-unstructured-script/run_regressor_evaluation.py
+++ oop-essentials/T01-unstructured-script/run_regressor_evaluation.py
@@ -3,15 +3,40 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
-
-from songpop.data import *
+import pandas as pd
+import numpy as np
 
 
 def main():
-    dataset = Dataset(10000)
-    X, y = dataset.load_xy_projected_scaled()
+
+    # data import and dummy variables
+    include_columns = ['popularity','genre', 'danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness',
+                   'acousticness','instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms',
+                   'time_signature']
+
+    df = pd.read_csv("../data/spotify_data_sample.csv", usecols=include_columns)
+
+    #df = df.sample(frac=0.1, replace=False, random_state=42)
+    #df.to_csv("../data/spotify_data_sample.csv")
+
+    numeric_data = df.select_dtypes(include=[np.number])
+    categorical_data = df.select_dtypes(exclude=[np.number])
+
+    # to dummies
+    for category in categorical_data:
+        dummy_df = pd.get_dummies(df[category])
+        df = df.join(dummy_df)
+    df.drop(categorical_data, inplace=True, axis=1)
+
+    target = 'popularity'
+    independent_variables = [var for var in df.columns.to_list() if var!=target]
+    X = df[independent_variables]
+    y = df[target]
+    print("Stage1 = Data Load")
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.3, shuffle=True)
+    print("Stage2 = Split")
+
 
     log_reg = linear_model.LogisticRegression(solver='lbfgs', max_iter=1000)
     log_reg.fit(X_train, y_train)
